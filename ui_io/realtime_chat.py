d49a4165096e782af9bbdaf8eb7cf54011e7bd22
abhinavamirtharaj-cpu <abhinavamirtharaj@gmail.com>
2026-01-10 10:31:22 +0000

    Simplify to automatic WhatsApp-like pairing system
    - Remove room code requirement
    - Automatically pair users when they connect
    - First user waits, second user gets paired automatically
    - Show partner connection status in real-time
    - Display partner name in header
    - Prevent sending messages until partner connects
    - Clean up disconnection handling for paired users
    - Simpler, more intuitive user experience

--- ui_io/realtime_chat.py
+++ ui_io/realtime_chat.py
@@ -27,8 +27,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', secrets.token_hex(32))
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 # Store active rooms and users
-active_rooms = {}  # {room_id: [user1_sid, user2_sid]}
-user_names = {}    # {sid: username}
+waiting_user = None  # Single user waiting to be paired: {'sid': sid, 'username': username}
+active_rooms = {}  # {room_id: {'user1': {'sid': sid, 'username': username}, 'user2': {...}}}
 user_rooms = {}    # {sid: room_id}
 
 
@@ -47,78 +47,102 @@ def handle_connect():
 @socketio.on('disconnect')
 def handle_disconnect():
     """Handle user disconnection"""
+    global waiting_user
     sid = request.sid
+    
+    # Check if disconnecting user was waiting
+    if waiting_user and waiting_user['sid'] == sid:
+        waiting_user = None
+        print(f'Waiting user disconnected: {sid}')
+        return
     
     # Leave room if in one
     if sid in user_rooms:
         room_id = user_rooms[sid]
         leave_room(room_id)
         
-        # Remove from active rooms
-        if room_id in active_rooms and sid in active_rooms[room_id]:
-            active_rooms[room_id].remove(sid)
+        # Notify partner
+        if room_id in active_rooms:
+            room_data = active_rooms[room_id]
+            partner_sid = None
+            disconnected_username = ''
             
-            # Notify other user
-            if active_rooms[room_id]:
-                emit('user_left', {
-                    'username': user_names.get(sid, 'Unknown')
-                }, room=room_id)
-            else:
-                del active_rooms[room_id]
+            if room_data['user1']['sid'] == sid:
+                partner_sid = room_data['user2']['sid']
+                disconnected_username = room_data['user1']['username']
+            elif room_data['user2']['sid'] == sid:
+                partner_sid = room_data['user1']['sid']
+                disconnected_username = room_data['user2']['username']
+            
+            if partner_sid:
+                emit('partner_left', {
+                    'username': disconnected_username
+                }, room=partner_sid)
+                
+                # Remove partner from room tracking
+                if partner_sid in user_rooms:
+                    del user_rooms[partner_sid]
+            
+            # Clean up room
+            del active_rooms[room_id]
         
         del user_rooms[sid]
-    
-    # Clean up user data
-    if sid in user_names:
-        del user_names[sid]
     
     print(f'Client disconnected: {sid}')
 
 
-@socketio.on('join_room')
-def handle_join_room(data):
-    """Handle user joining a room"""
-    room_id = data.get('room_id', 'default')
+@socketio.on('find_partner')
+def handle_find_partner(data):
+    """Match user with next available partner"""
+    global waiting_user
     username = data.get('username', 'Anonymous')
     sid = request.sid
     
-    # Store user info
-    user_names[sid] = username
-    user_rooms[sid] = room_id
-    
-    # Initialize room if doesn't exist
-    if room_id not in active_rooms:
-        active_rooms[room_id] = []
-    
-    # Check if room is full (max 2 users)
-    if len(active_rooms[room_id]) >= 2 and sid not in active_rooms[room_id]:
-        emit('room_full', {'error': 'Room is full (max 2 users)'})
-        return
-    
-    # Join the room
-    join_room(room_id)
-    if sid not in active_rooms[room_id]:
-        active_rooms[room_id].append(sid)
-    
-    # Send room info to user
-    emit('room_joined', {
-        'room_id': room_id,
-        'username': username,
-        'users_count': len(active_rooms[room_id])
-    })
-    
-    # Notify others in room
-    emit('user_joined', {
-        'username': username,
-        'users_count': len(active_rooms[room_id])
-    }, room=room_id, include_self=False)
-    
-    # Send chat history
-    contact = {'id': room_id, 'name': room_id}
-    history = get_history(room_id)
-    emit('chat_history', {'messages': history[-50:]})  # Last 50 messages
-    
-    print(f'User {username} ({sid}) joined room {room_id}')
+    # Check if someone is already waiting
+    if waiting_user and waiting_user['sid'] != sid:
+        # Pair them together
+        room_id = f"room_{waiting_user['sid'][:8]}_{sid[:8]}"
+        
+        # Create room with both users
+        active_rooms[room_id] = {
+            'user1': waiting_user,
+            'user2': {'sid': sid, 'username': username}
+        }
+        
+        # Join both to room
+        user_rooms[waiting_user['sid']] = room_id
+        user_rooms[sid] = room_id
+        
+        join_room(room_id, sid=waiting_user['sid'])
+        join_room(room_id, sid=sid)
+        
+        # Notify both users they're paired
+        emit('paired', {
+            'room_id': room_id,
+            'partner_name': username
+        }, room=waiting_user['sid'])
+        
+        emit('paired', {
+            'room_id': room_id,
+            'partner_name': waiting_user['username']
+        })
+        
+        # Send chat history
+        contact = {'id': room_id, 'name': room_id}
+        history = get_history(room_id)
+        if history:
+            emit('chat_history', {'messages': history[-50:]}, room=room_id)
+        
+        print(f'Paired {waiting_user["username"]} with {username} in room {room_id}')
+        
+        # Clear waiting user
+        waiting_user = None
+    else:
+        # No one waiting, this user becomes the waiting user
+        waiting_user = {'sid': sid, 'username': username}
+        user_rooms[sid] = None  # Not in a room yet
+        emit('waiting')
+        print(f'User {username} ({sid}) is waiting for a partner')
 
 
 @socketio.on('send_message')
